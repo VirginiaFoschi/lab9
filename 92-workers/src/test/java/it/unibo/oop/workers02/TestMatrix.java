5bf49208b6aefa83b728e9965222b97a5e728770
Virginia Foschi <virginia.foschi@studio.unibo.it>
2022-11-22 21:10:10 +0100

    modified with main method

--- 92-workers/src/test/java/it/unibo/oop/workers02/TestMatrix.java
+++ 92-workers/src/test/java/it/unibo/oop/workers02/TestMatrix.java
@@ -29,14 +29,14 @@ public class TestMatrix {
      * output, ad eccezione ovviamente dei tempi.
      */
 
-    private static final int SIZE = 1_000;
+    private static final int SIZE = 5;
     private static final double EXPECTED_DELTA = 0.01;
     private static final String MSEC = " msec";
 
     /**
      * Base test for a multithreaded matrix sum.
      */
-    @Test
+    /*@Test
     public void testBasic() {
         double sum = 0;
         final double[][] matrix = new double[SIZE][SIZE];
@@ -49,9 +49,32 @@ public class TestMatrix {
         System.out.println("BTW: the sum with " + SIZE + "*" + SIZE + " elements is: " + sum);
         long time;
         for (final int threads : new int[] { 1, 2, 3, 8, 16, 32, 100 }) {
-            final SumMatrix sumList = null; // new MultiThreadedSumMatrix(threads);
+            final SumMatrix sumList = new MultiThreadedSumMatrix(threads);
             time = System.nanoTime();
             assertEquals(sum, sumList.sum(matrix), EXPECTED_DELTA);
+            time = System.nanoTime() - time;
+            System.out.println("Tried with " + threads + " thread"
+                    + (threads == 1 ? "" : "s") + ": "
+                    + TimeUnit.NANOSECONDS.toMillis(time) + MSEC);
+        }
+    } */
+
+    public static void main(String[] args){
+        double sum = 0;
+        final double[][] matrix = new double[SIZE][SIZE];
+        for (double[] d : matrix) {
+            for (int i = 0; i < SIZE; i++) {
+                d[i] = i;
+                sum += i;
+            }
+        }
+        System.out.println("BTW: the sum with " + SIZE + "*" + SIZE + " elements is: " + sum);
+        long time;
+        for (final int threads : new int[] { 1, 2, 3 ,8}) {
+            final SumMatrix sumList = new MultiThreadedSumMatrix(threads);
+            time = System.nanoTime();
+            //assertEquals(sum, sumList.sum(matrix), EXPECTED_DELTA);
+            System.out.println("sum=" +sum+ " sum2="+ sumList.sum(matrix));
             time = System.nanoTime() - time;
             System.out.println("Tried with " + threads + " thread"
                     + (threads == 1 ? "" : "s") + ": "
